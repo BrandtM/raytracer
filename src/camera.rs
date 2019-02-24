c6b3d0db2766818714b16ab16d2413dce632c2e9
unknown <marius.brandt@hotmail.com>
2019-02-24 13:53:39 +0100

    Ch.6 antialiasing done

---
+++ src/camera.rs
@@ -0,0 +1,27 @@
+use cgmath::Vector3;
+use crate::ray::Ray;
+
+pub struct Camera {
+	pub origin: Vector3<f32>,
+	pub bottom_left: Vector3<f32>,
+	pub horizontal: Vector3<f32>,
+	pub vertical: Vector3<f32>,
+}
+
+impl Camera {
+	pub fn new(origin: Vector3<f32>, bottom_left: Vector3<f32>, horizontal: Vector3<f32>, vertical: Vector3<f32>) -> Camera {
+		Camera {
+			origin,
+			bottom_left,
+			horizontal,
+			vertical,
+		}
+	}
+
+	pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+		Ray {
+			origin: self.origin,
+			direction: self.bottom_left + u * self.horizontal + v * self.vertical - self.origin,
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
 
 extern crate cgmath;
 
-use std::fs::File;
-use std::io::prelude::*;
-use cgmath::Vector3;
-
 pub mod hitable;
 pub mod hitable_list;
 pub mod sphere;
 pub mod ray_hit;
 pub mod ray;
+pub mod camera;
 
-use ray::Ray;
+use std::fs::File;
+use std::io::prelude::*;
+use cgmath::Vector3;
+use rand::Rng;
+
 use sphere::Sphere;
 use hitable_list::HitableList;
+use camera::Camera;
 
 fn main() {
     let mut file = File::create("image.ppm").unwrap();
 
     let wx = 200;
     let wy = 100;
+    let samples_per_pixel = 100;
 
     file.write(format!("P3\n{} {}\n255\n", wx, wy).as_bytes()).unwrap();
 
-    let bottom_left = Vector3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vector3::new(4.0, 0.0, 0.0);
-    let vertical = Vector3::new(0.0, 2.0, 0.0);
-    let origin = Vector3::new(0.0, 0.0, 0.0);
+    let camera = Camera::new(
+        Vector3::new(0.0, 0.0, 0.0), 
+        Vector3::new(-2.0, -1.0, -1.0), 
+        Vector3::new(4.0, 0.0, 0.0), 
+        Vector3::new(0.0, 2.0, 0.0)
+    );
 
     let sphere1 = Sphere {
         center: Vector3::new(0.0, 0.0, -1.0),
@@ -42,16 +47,21 @@ fn main() {
         list: vec![Box::new(sphere1), Box::new(sphere2)],
     };
 
+    let mut rng = rand::thread_rng();
+
     for y in (0..wy).rev() {
         for x in 0..wx {
-            let u = x as f32 / wx as f32;
-            let v = y as f32 / wy as f32;
-            let ray = Ray {
-                origin,
-                direction: bottom_left + u * horizontal + v * vertical
-            };
+            let mut color = Vector3::new(0.0, 0.0, 0.0);
 
-            let color = ray.color(&hitable_list);
+            for _sample in 0..samples_per_pixel {
+                let u = (x as f32 + rng.gen::<f32>()) / wx as f32;
+                let v = (y as f32 + rng.gen::<f32>()) / wy as f32;
+                let ray = camera.get_ray(u, v);
+                color += ray.color(&hitable_list);
+            }
+
+            color /= samples_per_pixel as f32;
+
             let r = (255.0 * color.x) as u8;
             let g = (255.0 * color.y) as u8;
             let b = (255.0 * color.z) as u8;
